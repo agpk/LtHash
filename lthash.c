7295885e0e142b3c4df0fd40ba51fe2c118fa9b5
agpk <109487033+agpk@users.noreply.github.com>
2022-07-24 00:51:14 +0200

    Add files via upload

---
+++ lthash.c
@@ -0,0 +1,21 @@
+
+//              LtHash Library
+//     Copyright (c) Alan Kubiak, 2022.
+//
+//  This project is under the GPLv3 license.
+//           For the LLEC project.
+//
+//   --------------------------------------
+//       Implemented LiteHash algorithm
+//   --------------------------------------
+
+int lthash(char* c) {
+    int d=0;
+    for(int i=0,e=2;i<strlen(c);i++,e++) {
+        d+=c[i]*e;
+        if(e==0x7) {
+            e=1;
+        }
+    }
+    return d;
+}
